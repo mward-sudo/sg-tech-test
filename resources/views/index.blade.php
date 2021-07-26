6ca2f7361a97561b765416c3270941b915953bc2
Michael Ward <mward82@gmail.com>
2021-07-26 02:11:44 +0100

    Basic styling for upload

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,7 @@
 <x-layout title="Upload your homeowners file">
-    <div>
-        <p>Upload your homeowner names file:</p>
+    <div class="grid justify-center items-center h-screen">
+        <div class="shadow-lg text-center p-8 self-center justify-self-center rounded-lg">
+            <h1 class="text-4xl">Upload your home owner names file</h1>
+        </div>
     </div>
 </x-layout>
